55f9e984f0b560e1ab0ea59ee2e97f340c301515
Aditya-567 <lextrone000567@gmail.com>
2025-08-22 19:20:54 +0530

    home

--- TBI/src/app/dashboard/dashboard/dashboard.component.ts
+++ TBI/src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,8 +1,6 @@
-import { Component, ElementRef, EventEmitter, OnInit, Output, signal, ViewChild } from '@angular/core';
-import { trigger, transition, style, animate } from '@angular/animations';
-import { BottomBarService } from '../../shared/header/bottom-bar.service';
-import { AuthService } from '../../login/auth-service.service';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../../login/auth-service.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -37,24 +35,95 @@ export class DashboardComponent implements OnInit {
   featureCards = [
     {
       icon: 'ifc.svg',
-      title: 'Enhance Profile Pic',
-      description: 'Write, edit, and execute code effortlessly.',
+      title: 'Profile Enhancement',
+      description: 'Improve and retouch profile pictures with AI precision.',
     },
     {
       icon: 'dfc.svg',
-      title: 'Access it AnyTime',
-      description: 'Invite team members and set up roles in just a few clicks.',
+      title: 'Access Anytime',
+      description: 'Save and edit your images from anywhere, anytime.',
     },
     {
       icon: 'sfc.svg',
-      title: 'AI',
+      title: 'AI-Powered Tools',
       description:
-        'Powered by OnceHub, CodeOnce ensures lightning-fast execution.',
+        'Leverage advanced AI for editing, enhancing, and restoring images.',
     },
     {
       icon: 'tfc.svg',
       title: 'Add Text',
-      description: 'Visualize user progress and detailed reports.',
+      description: 'Place stylish and customizable text on your images.',
+    },
+    {
+      icon: 'restore.svg',
+      title: 'Image Restore',
+      description: 'Bring old or damaged photos back to life instantly.',
+    },
+    {
+      icon: 'remove.svg',
+      title: 'Object Removal',
+      description: 'Erase unwanted objects seamlessly from any picture.',
+    },
+    {
+      icon: 'stars.svg',
+      title: 'Generative Fill',
+      description: 'Fill missing areas or extend backgrounds naturally.',
+    },
+    {
+      icon: 'recolor.svg',
+      title: 'Object Recolor',
+      description: 'Change object colors while keeping natural textures.',
+    },
+  ];
+
+  featureCardsSecond = [
+    {
+      icon: 'social.svg',
+      title: 'Enhance Profile Picture',
+    },
+    {
+      icon: 'pic.svg',
+      title: 'Edit Images Anywhere',
+    },
+    {
+      icon: 'aistart.svg',
+      title: 'AI-Powered Image Enhancement',
+    },
+    {
+      icon: 'ui.svg',
+      title: 'Simple, intuitive, and fast editing experience.',
+    },
+    {
+      icon: 'pay.svg',
+      title: 'Secure & Reliable Payments',
+    },
+  ];
+
+  steps = [
+    {
+      number: '1',
+      Image: 'service-h.svg',
+      title: 'Select Service',
+      description: 'Add and style your text layers',
+    },
+    {
+      number: '2',
+      Image: 'upload-h.svg',
+      title: 'Upload Photo',
+      description: 'Upload any image from your device',
+    },
+
+    {
+      number: '3',
+      Image: 'edit-h.svg',
+      title: 'Edit the Image',
+      description: 'Place text behind image elements',
+    },
+    {
+      number: '4',
+      Image: 'download-h.svg',
+      title: 'Download',
+      description: 'Export in high quality PNG format',
     },
   ];
 }
