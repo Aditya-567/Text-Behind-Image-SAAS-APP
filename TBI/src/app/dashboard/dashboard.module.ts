825c297637927616dbb60b5fa50ca1ca12300827
Aditya-567 <lextrone000567@gmail.com>
2025-08-30 21:08:13 +0530

    Refactor code structure for improved readability and maintainability

--- TBI/src/app/dashboard/dashboard.module.ts
+++ TBI/src/app/dashboard/dashboard.module.ts
@@ -3,16 +3,17 @@ import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { MatTabsModule } from '@angular/material/tabs';
-import { RippleGridComponent } from './ripple-grid/ripple-grid.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
 @NgModule({
-  declarations: [DashboardComponent, RippleGridComponent],
+  declarations: [DashboardComponent],
   imports: [
     CommonModule,
     DashboardRoutingModule,
     MatTabsModule,
-  ]
+    SharedModule
+  ],
 })
 export class DashboardModule { }
